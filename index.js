d6a13f9ac9d0bc984341cd82a8f73a1bf387c677
GerardoPrz <60196783+GerardoPrz@users.noreply.github.com>
2024-02-07 17:21:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ admin.initializeApp({
 //CORS
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  /*res.header(
-    "Acces-Control-Allow-Headers",
+  res.header(
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );*/
+  );
 
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
